3a335d826497a5f193a5bb9b1d1d24e4b023eb77
Phil Vargas <philipvargas@thisisartium.com>
2022-07-12 14:39:36 -0700

    feat - add xcode project

---
+++ twenty-fourty-eightUITests/twenty_fourty_eightUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  twenty_fourty_eightUITestsLaunchTests.swift
+//  twenty-fourty-eightUITests
+//
+//  Created by Phil Vargas on 7/12/22.
+//
+
+import XCTest
+
+class twenty_fourty_eightUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
